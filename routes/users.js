0cec30d63865cb679e8bdc5a7764cc2e7889f055
richardmundyiii <mundy.richard@icloud.com>
2023-12-24 23:48:02 -0700

    signup logic

---
+++ routes/users.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const router = express.Router();
+const userCtrl = require("../controllers/users");
+const ensureLoggedIn = require("../config/ensureLoggedIn");
+
+router.post("/", userCtrl.create);
+
+router.post("/login", userCtrl.login);
+
+router.get("/check-token", ensureLoggedIn, userCtrl.checkToken);
